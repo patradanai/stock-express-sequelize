917cabeb83e9b60c11893126781abffa0087cadb
unknown <bloody_jojo@hotmail.com>
2021-04-14 22:43:24 +0700

    completed product controller

--- src/controllers/Product.controller.ts
+++ src/controllers/Product.controller.ts
@@ -56,17 +56,53 @@ const updateProduct = async (req: ResUser, res: Response) => {
   const { product, productDesc, productPrice, isActive, supplierId } = req.body;
 
   try {
+    // Check owner Product with UserId
     const productUpdate = await Product.findByPk(id);
     if (productUpdate.userId != userId) {
       return res
         .status(401)
-        .json({ message: "Authorization for this product" });
+        .json({ message: "UnAuthorization for this product" });
     }
+
+    /// Find Supplier
+    const supplierInstance = await Supplier.findByPk(supplierId);
+
+    // Update Product
+    await Product.update(
+      {
+        product: product,
+        productPrice: productPrice,
+        productDesc: productDesc,
+        isActive: isActive,
+        supplierId: supplierInstance.id,
+      },
+      { where: { id: id } }
+    );
+
+    return res.status(200).json({ message: `Completed update Id ${id}` });
   } catch (err) {
     return res.status(500).json({ Error: err.message });
   }
 };
 
-const deleteProduct = (req: Request, res: Response) => {};
+const deleteProduct = async (req: ResUser, res: Response) => {
+  const userId = req.userId;
+  const { id } = req.params;
+
+  try {
+    // Check owner Product with UserId
+    const productDelete = await Product.findByPk(id);
+    if (productDelete.userId != userId) {
+      return res
+        .status(401)
+        .json({ message: "UnAuthorization for this product" });
+    }
+
+    await Product.destroy({ where: { id: id } });
+    return res.status(200).json({ message: `Deleted Id ${id} Completed` });
+  } catch (err) {
+    return res.status(500).json({ Error: err.message });
+  }
+};
 
 export { readProducts, createProduct, updateProduct, deleteProduct };
